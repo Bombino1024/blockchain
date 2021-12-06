b8243864595f1f2d4f47b881b03f2d8d4b6f5b19
Martin Bombala <xbombalam@stuba.sk>
2021-12-06 09:11:21 +0100

    deploy lottery

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var TodoList = artifacts.require("./TodoList.sol");
+var BombiToken = artifacts.require("./BombiToken.sol");
 
 module.exports = function (deployer) {
   deployer.deploy(TodoList);
+  deployer.deploy(BombiToken, 21000000);
 };
