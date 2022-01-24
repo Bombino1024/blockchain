7c82d2300056e5528778004e33982728531ce72e
Martin Bombala <xbombalam@stuba.sk>
2022-01-24 07:52:57 +0100

    delete

--- smart-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var TodoList = artifacts.require("./TodoList.sol");
-var BombiToken = artifacts.require("./BombiToken.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(TodoList);
-  deployer.deploy(BombiToken, 21000000);
-};
